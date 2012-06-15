ca39ba5bec947731de330cf474515b678c390a6a
Ellipsis753 <shanee753@gmail.com>
2012-06-15 01:45:11 +0100

    Window title, code refactorying, styling. Some error catching. Other stuff. Lots of small style changes. Make sure not to use magic numbers in future. Spaces added before "{" for consistancy. No one line ifs. Sdl should be included "SDL.h", note the quotes and lack of SDL/ before it. We need to have more error catching, I added "file not found" catching.

---
+++ src/defines.h
@@ -0,0 +1,18 @@
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#define SDL_INIT_ARGS SDL_INIT_VIDEO|SDL_INIT_JOYSTICK
+
+#define SDL_AUDIO_RATE 22050
+#define SDL_AUDIO_FORMAT AUDIO_S16SYS
+#define SDL_AUDIO_CHANNELS 2
+#define SDL_AUDIO_BUFFER 4096
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+//use default screen bpp. It's faster.
+#define WINDOW_BPP 0
+#define WINDOW_TITLE "IWBTOR"
+#define WINDOW_ICON_TEXT "IWBTOR"
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "CApp.h"
+
+int main(int argc, char* args[]) {
+    CApp theApp;
+    return theApp.OnExecute();
+}
+
